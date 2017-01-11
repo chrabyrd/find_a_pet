6b7fdb4085d1fdf684b016601d7f0aa28168fc21
Christopher <chrabyrd@gmail.com>
2017-01-10 18:20:58 -0800

    Finish frontend/backend user auth

--- frontend/find_a_pet.jsx
+++ frontend/find_a_pet.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { login } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.user) {
+    const preloadedState = { session: { user: window.user } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
-
-  window.store = store;
-  window.login = login;
 });
