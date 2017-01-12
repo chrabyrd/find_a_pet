bdf172c4855cdc684e7b7f89eb7bc1d306d272cf
Christopher <chrabyrd@gmail.com>
2017-01-12 12:18:47 -0800

    Add demo account seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'Demo Account', email: 'demo@demo.demo', password: 'password')
