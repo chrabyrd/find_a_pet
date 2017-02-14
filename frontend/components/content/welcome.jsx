1e6c826ce59eeb5cde58e1422bb03576b57987c5
Christopher <chrabyrd@gmail.com>
2017-02-13 16:59:20 -0800

    Refactor header

--- frontend/components/content/welcome.jsx
+++ frontend/components/content/welcome.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
 import PetIndexContainer from '../pets/pet_index_container';
-import PetSlider from './petSlider';
 
 
 const Welcome = () => (
 
   <div className="welcome-container">
 
-    <div>
-      <PetSlider />
-    </div>
-
-    <div>
-      <h1>Pets in your area:</h1>
+    <div className="local-pets-container">
+      <h1 className="local-pets">Pets in your area:</h1>
       <PetIndexContainer />
     </div>
 
--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -2,23 +2,40 @@ import React from 'react';
 import { Link } from 'react-router';
 import GreetingContainer from './greeting_container';
 import SearchBarContainer from './search_bar_container';
+import PetSlider from '../content/petSlider';
+import Modal from 'react-modal';
 
-const Header = () => (
-  <div className="header">
-    <div className="header-info">
-      <Link to="/" className="header-logo">
-        <div className="header-title">
-          <h2>Find A Pet!</h2>
+const Header = () => {
+
+  return (
+    <div id="header" className="header">
+      <Modal
+        className="header-modal"
+        overlayClassName="header-overlay"
+        isOpen={true}
+        parentSelector={() => (
+          document.getElementById("header")
+        )}
+        contentLabel="header-modal"
+      >
+        <div className="header-info">
+          <Link to="/" className="header-logo">
+            <div className="header-title">
+              <h2>Find A Pet!</h2>
+            </div>
+          </Link>
+          <GreetingContainer />
         </div>
-      </Link>
-      <GreetingContainer />
-    </div>
 
-    <div>
-      <SearchBarContainer />
+        <SearchBarContainer />
+
+      </Modal>
+
+      <PetSlider />
+
     </div>
-  </div>
-);
+  );
+};
 
 
 export default Header;
